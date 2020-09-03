c24e5a43967968dae8c313c7d1d687823eb7713c
Hamza Ali Zahid <34899077+HamzaAliPch@users.noreply.github.com>
2020-09-03 16:46:59 +0500

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+clear
+apt update
+apt upgrade
+apt install figlet -y
+figlet Basic Installation
+apt install toilet -y
+apt install cowsay -y
+apt install nano -y
+apt install ruby -y
+gem install lolcat
+apt install git curl openssh openssl openssl-tool -y
+figlet -f big FINISHED !!! | lolcat
+xdg-open https://www.youtube.com/channel/UCfrrGnYOMI28lLM1TpA4aPw?sub_confirmation=1
+echo
+echo -e "\e[1m Now Run bash instacracker.sh...!!!"
+echo
+echo -e "\e[1m\e[32m Developed by :\e[33m Tech Cochi"
